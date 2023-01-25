7bbfa7b5de93a6a9763fd4a46788ce2e6d8acd97
matteoggg <95472288+matteoggg@users.noreply.github.com>
2023-01-25 20:19:55 +0100

    gestione utente completa

--- lib/queries.c
+++ lib/queries.c
@@ -40,6 +40,18 @@ queryResult_t* query_router(MYSQL* con, char* command, char** param, int length)
   else if(strncmp(command, "getPreferences", strlen(command)) == 0) {
       return getPreferences(con, param, length);
   }
+  else if(strncmp(command, "updateUserInfo", strlen(command)) == 0) {
+      return updateUserInfo(con, param, length);
+  }
+  else if(strncmp(command, "updatePassword", strlen(command)) == 0) {
+      return updatePassword(con, param, length);
+  }
+  else if(strncmp(command, "getUserInfo", strlen(command)) == 0) {
+      return getUserInfo(con, param, length);
+  }
+  else if(strncmp(command, "deleteUser", strlen(command)) == 0) {
+      return deleteUser(con, param, length);
+  }
   else if (strncmp(command, "exit", strlen(command)) == 0) {
     queryResult_t* res = (queryResult_t*) malloc(sizeof(queryResult_t));
     res->error = 6969;
@@ -167,3 +179,99 @@ queryResult_t* getPreferences(MYSQL* con, char** param, int length){
     printf("QUERY COMPOSTA FINALE: %s\n", query);
     return res;
 }
+
+queryResult_t* updateUserInfo(MYSQL* con, char** param, int length){
+
+    queryResult_t* res = (queryResult_t*) malloc(sizeof(queryResult_t));
+    res->error = 0;
+    res->result = NULL;
+
+    char* query = "UPDATE users AS u SET u.nome = '";
+    query = concat(query, param[1]);
+    query = concat(query, "', u.cognome = '");
+    query = concat(query, param[2]);
+    query = concat(query, "', u.phone = '");
+    query = concat(query, param[3]);
+    query = concat(query, "', u.address = '");
+    query = concat(query, param[4]);
+    query = concat(query, "', u.cap = '");
+    query = concat(query, param[5]);
+    query = concat(query, "', u.city = '");
+    query = concat(query, param[6]);
+    query = concat(query, "', u.province = '");
+    query = concat(query, param[7]);
+    query = concat(query, "' WHERE (u.username = '");
+    query = concat(query, param[0]);
+    query = concat(query, "')");
+
+    if (mysql_query(con, query)) {
+        res->error = print_and_return_error(con);
+        return res;
+    }
+
+    printf("QUERY COMPOSTA FINALE: %s\n", query);
+    return res;
+}
+
+queryResult_t* updatePassword(MYSQL* con, char** param, int length){
+
+    queryResult_t* res = (queryResult_t*) malloc(sizeof(queryResult_t));
+    res->error = 0;
+    res->result = NULL;
+
+    char* query = "UPDATE users AS u SET u.passw = '";
+    query = concat(query, param[1]);
+    query = concat(query, "' WHERE u.username = '");
+    query = concat(query, param[0]);
+    query = concat(query, "'");
+
+    if (mysql_query(con, query)) {
+        res->error = print_and_return_error(con);
+        return res;
+    }
+
+    printf("QUERY COMPOSTA FINALE: %s\n", query);
+    return res;
+}
+
+queryResult_t* getUserInfo(MYSQL* con, char** param, int length){
+
+    queryResult_t* res = (queryResult_t*) malloc(sizeof(queryResult_t));
+    res->error = 0;
+    res->result = NULL;
+
+    char* query = "SELECT u.nome, u.cognome, u.phone, u.address, u.cap, u.city, u.province FROM users AS u WHERE u.username = '";
+    query = concat(query, param[0]);
+    query = concat(query, "'");
+
+    if (mysql_query(con, query)) {
+        res->error = print_and_return_error(con);
+        return res;
+    }
+
+    MYSQL_RES* result = mysql_store_result(con);
+    res->result = result;
+
+    printf("QUERY COMPOSTA FINALE: %s\n", query);
+    return res;
+}
+
+queryResult_t* deleteUser(MYSQL* con, char** param, int length){
+
+    queryResult_t* res = (queryResult_t*) malloc(sizeof(queryResult_t));
+    res->error = 0;
+    res->result = NULL;
+
+    char* query = "DELETE FROM users AS u WHERE u.username = '";
+    query = concat(query, param[0]);
+    query = concat(query, "'");
+
+    if (mysql_query(con, query)) {
+        res->error = print_and_return_error(con);
+        return res;
+    }
+
+    printf("QUERY COMPOSTA FINALE: %s\n", query);
+    return res;
+}
+
--- lib/queries.h
+++ lib/queries.h
@@ -18,6 +18,10 @@ queryResult_t* signup(MYSQL*, char**, int);
 queryResult_t* login(MYSQL*, char**, int);
 queryResult_t* updatePreferences(MYSQL*, char**, int);
 queryResult_t* getPreferences(MYSQL*, char**, int);
+queryResult_t* updateUserInfo(MYSQL*, char**, int);
+queryResult_t* updatePassword(MYSQL*, char**, int);
+queryResult_t* getUserInfo(MYSQL*, char**, int);
+queryResult_t* deleteUser(MYSQL*, char**, int);
 queryResult_t* query_router(MYSQL*,char*, char**, int);
 
 
